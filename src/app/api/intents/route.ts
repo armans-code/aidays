4268b009332f0f21bbc5a9ff961a4ace15567b48
Arman <arman.kumaraswamy@gmail.com>
2024-10-25 19:44:09 -0400

    fix: check that Authorization header exists in /api/intents

--- src/app/api/intents/route.ts
+++ src/app/api/intents/route.ts
@@ -2,9 +2,13 @@ import { verify } from "jsonwebtoken";
 
 export async function POST(req: Request) {
   // Verify webhook
-  const token = req.headers["Authorization"] as string;
+  const token = req.headers.get("Authorization");
+  if (!token) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   try {
-    const decoded = verify(token, process.env.WEBHOOK_SECRET);
+    const decoded = verify(token, process.env.WEBHOOK_SECRET as string);
   } catch (error) {
     return new Response("Unauthorized", { status: 401 });
   }
