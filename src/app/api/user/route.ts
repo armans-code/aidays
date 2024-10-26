f4d4d06ee8d8515e87d1dd504bc598bc73174f18
Arman <arman.kumaraswamy@gmail.com>
2024-10-26 02:17:07 -0400

    Clean up webhook route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -12,23 +12,20 @@ const bodySchema = z.object({
 // TODO: add rate limiting with KV
 
 export async function POST(req: Request) {
-  //   TODO: Add webhook verification
-  //   const token = req.headers.get("Authorization");
-  //   if (!token) {
-  //     return new Response("Unauthorized", { status: 401 });
-  //   }
-  //   try {
-  //     const decoded = verify(token, process.env.WEBHOOK_SECRET as string);
-  //   } catch (error) {
-  //     return new Response("Unauthorized", { status: 401 });
-  //   }
+  // TODO: Add webhook verification
 
-  // save data to db
   const body = await req.json();
   try {
     const res = bodySchema.parse(body);
-    await db.insert(wants).values(res);
+    try {
+      await db.insert(wants).values(res);
+    } catch (error) {
+      return new Response("Internal Server Error", { status: 500 });
+    }
+    return new Response("OK", { status: 201 });
   } catch (error) {
     return new Response("Bad Request", { status: 400 });
   }
+
+  // i need to learn effect :(
 }
