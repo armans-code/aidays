d13cf3c5626fdaa9d4b108fd29aa9a6fca910ff1
Arman <arman.kumaraswamy@gmail.com>
2024-10-26 19:27:46 -0400

    Test severity.ts

--- src/lib/severity.ts
+++ src/lib/severity.ts
@@ -1,18 +1,19 @@
-import { WatsonXAI } from '@ibm-cloud/watsonx-ai';
-import { IamAuthenticator } from 'ibm-cloud-sdk-core'; // Import the IamAuthenticator
+import { WatsonXAI } from "@ibm-cloud/watsonx-ai";
+import { IamAuthenticator } from "ibm-cloud-sdk-core";
 
 // Initialize WatsonXAI service with proper authenticator
 const watsonxAIService = WatsonXAI.newInstance({
-  version: '2024-05-31',
-  serviceUrl: 'https://us-south.ml.cloud.ibm.com',
+  version: "2024-05-31",
+  serviceUrl: "https://us-south.ml.cloud.ibm.com",
   authenticator: new IamAuthenticator({
-    apikey: process.env.WATSONX_AI_APIKEY || ''
+    apikey: process.env.WATSONX_AI_APIKEY!,
   }),
 });
 
 const params = {
-  input: 'Classify the natural disaster severity level on a scale from 0-10. 10 being highest severity\n\nInput: {input_text}\nOutput:',
-  modelId: 'google/flan-ul2',
+  input:
+    "Classify the natural disaster severity level on a scale from 0-10. 10 being highest severity. Only return the value and nothing else.\n\nInput: {input_text}\nOutput:",
+  modelId: "google/flan-ul2",
   parameters: {
     max_new_tokens: 100,
   },
@@ -22,17 +23,24 @@ export async function classifySeverity(inputText: string): Promise<number> {
   try {
     const updatedParams = {
       ...params,
-      input: params.input.replace('{input_text}', inputText),
-      projectId: process.env.WATSONX_AI_PROJECT_ID
+      input: params.input.replace("{input_text}", inputText),
+      projectId: process.env.WATSONX_AI_PROJECT_ID!,
     };
-    const response = await watsonxAIService.generateText(updatedParams);
+    const response = await watsonxAIService.generateText({
+      projectId: process.env.WATSONX_AI_PROJECT_ID!,
+      input: "There is a wildfire in my neighborhood and I'm stuck in my home.",
+      modelId: "google/flan-ul2",
+    });
     console.log({ response: response.result.results[0].generated_text });
     return parseInt(response.result.results[0].generated_text, 10);
   } catch (err) {
-    console.error('Error generating text:', err);
+    console.error("Error generating text:", err);
     throw err;
   }
 }
 
-// Example usage:
-// classifySeverity("A category 5 hurricane is approaching a densely populated coastal city.");
+console.log(
+  await classifySeverity(
+    "There is a wildfire in my neighborhood and I'm stuck in my home."
+  )
+);
