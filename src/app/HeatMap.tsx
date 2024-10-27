4ce301f35de631744c3e575f33df3a0ccd97df31
Jtolen543 <Jtolen543@gmail.com>
2024-10-26 20:34:23 -0400

    Updated Heat Map

--- src/app/HeatMap.tsx
+++ src/app/HeatMap.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React, { useEffect } from 'react';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
@@ -32,11 +32,11 @@ function demoData(): HeatmapDataPoint {
     const minLong = -82.4038;
     const maxLong = -82.2458;
 
-    const randLat = Math.random() * (maxLat - minLat) + minLat;
-    const randLong = Math.random() * (maxLong - minLong) + minLong;
-    const randSev = Math.random() * 11;
+  const randLat = Math.random() * (maxLat - minLat) + minLat;
+  const randLong = Math.random() * (maxLong - minLong) + minLong;
+  const randSev = Math.random() * 11;
 
-    return [randLat, randLong, randSev];
+  return [randLat, randLong, randSev];
 }
 
 // Generate 1000 random data points
@@ -46,41 +46,44 @@ for (let i = 0; i < 1000; i++) {
 
 // Main HeatMap component
 const HeatMap: React.FC = () => {
-    return (
-        <MapContainer center={[29.6516, -82.3248]} zoom={13} style={{ height: '500px', width: '100%' }}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <HeatmapLayer data={randData} />
-        </MapContainer>
-    );
+  return (
+    <MapContainer
+      center={[29.6516, -82.3248]}
+      zoom={13}
+      style={{ height: "500px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <HeatmapLayer data={randData} />
+    </MapContainer>
+  );
 };
 
 // HeatmapLayer component that adds the heat layer to the map
 interface HeatmapLayerProps {
-    data: HeatmapDataPoint[];
+  data: HeatmapDataPoint[];
 }
 
 const HeatmapLayer: React.FC<HeatmapLayerProps> = ({ data }) => {
-    const map = useMap();
+  const map = useMap();
 
-    useEffect(() => {
-        // Create and add the heatmap layer
-        const heatLayer = L.heatLayer(data, {
-            radius: 17,
-            blur: 10,
-            maxZoom: 17.0,
-            max: 1.0,
-        }).addTo(map);
+  useEffect(() => {
+    // Create and add the heatmap layer
+    const heatLayer = L.heatLayer(data, {
+      radius: 17,
+      blur: 10,
+      maxZoom: 17,
+      max: 1,
+    }).addTo(map);
+    // Cleanup: remove the heat layer when the component unmounts
+    return () => {
+      map.removeLayer(heatLayer);
+    };
+  }, [data, map]);
 
-        // Cleanup: remove the heat layer when the component unmounts
-        return () => {
-            map.removeLayer(heatLayer);
-        };
-    }, [data, map]);
-
-    return null; // This component only adds the heatmap layer, no visible elements
+  return null; // This component only adds the heatmap layer, no visible elements
 };
 
 export default HeatMap;
